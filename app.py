9ca9ede2bc3fa1b7cefe64c720eeda2af23a1c6c
Fedor Soldatkin <forkcs@yandex.ru>
2022-12-28 19:23:21 +0300

    Rename users bp url

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from blueprints.users import users_blueprint
 app = Flask(__name__)
 
 app.register_blueprint(admin_blueprint, url_prefix='/admin')
-app.register_blueprint(users_blueprint, url_prefix='/user')
+app.register_blueprint(users_blueprint, url_prefix='/users')
 
 if __name__ == '__main__':
     app.run(debug=True)
