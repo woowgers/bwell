7179495879630ad0e78edc1737f7fe2c30c77d86
woowgers <sashashibaev@gmail.com>
2023-03-17 15:10:17 +0300

    prettify using black

--- src/app/config.py
+++ src/app/config.py
@@ -9,9 +9,9 @@ load_dotenv("../../.env")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DATABASE = {
-    'host': os.environ.get("POSTGRES_HOST"),
-    'port': os.environ.get("POSTGRES_PORT"),
-    'database': os.environ.get("POSTGRES_DB"),
-    'user': os.environ.get("POSTGRES_USER"),
-    'password': os.environ.get("POSTGRES_PASSWORD")
+    "host": os.environ.get("POSTGRES_HOST"),
+    "port": os.environ.get("POSTGRES_PORT"),
+    "database": os.environ.get("POSTGRES_DB"),
+    "user": os.environ.get("POSTGRES_USER"),
+    "password": os.environ.get("POSTGRES_PASSWORD"),
 }
